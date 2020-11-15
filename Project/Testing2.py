f621879469553d170f06d59714cc7f5f6dca3bd3
Dishant <25dishant@gmail.com>
2020-11-15 21:13:28 +0530

    Random changes

--- Project/Testing2.py
+++ Project/Testing2.py
@@ -1,13 +1,5 @@
 import numpy as np
-Sobel_kernelX = np.array([[1, 3, -1],
-                          [2, 8, -2],
-                          [1, 25, -1]])
+import cv2 as cv
 
-y = Sobel_kernelX**2
-print(y)
 
-m = Sobel_kernelX*Sobel_kernelX
-print(m)
-
-n = np.matmul(Sobel_kernelX, Sobel_kernelX)
-print(n)
+cv.erode()
