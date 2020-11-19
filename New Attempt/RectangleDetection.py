01048ffaea39fe7bf2e303de4028b8f56b73519d
Dishant <25dishant@gmail.com>
2020-11-19 13:47:26 +0530

    Minor Change

--- New Attempt/RectangleDetection.py
+++ New Attempt/RectangleDetection.py
@@ -30,7 +30,7 @@ lines = cv.HoughLinesP(edgeImage, 1, np.pi/180, 50)
 
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    cv.line(image, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    cv.line(image, (x1, y1), (x2, y2), (0, 0, 255), 1)
 
 
 coutours, hierarchy = cv.findContours(
